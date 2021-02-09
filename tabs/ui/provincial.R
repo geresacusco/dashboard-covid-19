5c962b40feb073bd839a6631b0d04e277669a0de
Jason10121998 <164468@unsaac.edu.pe>
2021-02-09 10:53:22 -0500

    Correcciones al Dashboard
    Hice algunas correcciones en cuanto a los resúmenes. Básicamente agregué resúmenes al nivel provincial y distrital, además, actualicé el resumen para la gráfica de positividad molecular que tenía el mismo resumen de la gráfica de casos, todas estas en el semáforo.
    
    Finalmente, para le nivel provincial y distrital quité la gráfica 4 porque era repetitiva. En cambio, la variable de inicio de síntomas la puse en la gráfica 3, de esa manera juntamos 2 gráficos en 1.
    
    Por favor, redondea los valores en el semáforo y en la positividad molecular que se vean porcentajes.

--- tabs/ui/provincial.R
+++ tabs/ui/provincial.R
@@ -13,17 +13,77 @@ provincial <- tabPanel(title = "Nivel Provincial",
                        tabBox(width = 12,
                          title = h4(strong("Semáforo COVID")),
                          id = "tab_semaforo_prov",
-                         tabPanel("Tasa de positividad molecular",dygraphOutput("dygraph_prov_positividad_molecular")),
-                         tabPanel("Casos",dygraphOutput("dygraph_prov_new_cases")),
-                         tabPanel("Defunciones",dygraphOutput("dygraph_prov_new_deaths")),
-                         tabPanel("Comparativo Casos",dygraphOutput("dygraph_prov_comparativo_casos")),
-                         tabPanel("Comparativo Defunciones",dygraphOutput("dygraph_prov_comparativo_defunciones")),
-                         tabPanel("Comparativo Positividad Molecular",dygraphOutput("dygraph_prov_comparativo_posimolecular")))
+                         tabPanel("Tasa de positividad molecular",
+                                  tabsetPanel(
+                                    tabPanel("Gráfico", dygraphOutput("dygraph_prov_positividad_molecular"),
+                                             h4(strong("Descripción de los ejes")),
+                                             p(div(strong("Eje Y: "), em("Tasa de positividad de pruebas moleculares (promedio de 7 días)."), style = "color:blue")),
+                                             p(div(strong("Eje X: "), em("Días."), style = "color:blue"), "El primer día de la serie corresponde al 13/03/2020, fecha en la cual se reportó
+                                          el primer caso confirmado por COVID-19 en la región.")
+                                          ),
+                                    tabPanel("Resumen", "El semáforo COVID-19 de tasa de positividad molecular muestra
+                               el nivel de riesgo respecto al número total de personas infectadas por muestras moleculares procesadas
+                               por COVID-19.", br(), br(),
+                                             p("El color", strong("rojo"), "representa un nivel elevado de riesgo, en esta zona
+las precauciones aumentan. En esta zona el nivel y velocidad de contagio por muestras moleculares es mucho más elevada.
+Se recomienda salir de casa solo en casos excepcionales y tomando muy en cuenta las medidas
+de seguridad sanitaria. ", br(), br(),
+                                               "El color", strong("amarillo"), "representa un nivel de riesgo moderado. Aunque el riesgo aún se mantiene, se pueden realizar más
+actividades, siempre tomando en consideración las medidas de seguridad sanitaria.", br(), br(),
+                                               "El color", strong("verde"), "representa que el nivel de riesgo no es tan elevado respecto a los
+otros colores. En todo momento se deberían tomar en cuenta las medidas de seguridad sanitaria. 
+"))
+                                  )),
+                         tabPanel("Casos",
+                                  tabsetPanel(
+                                    tabPanel("Gráfico", dygraphOutput("dygraph_prov_new_cases"),
+                                             h4(strong("Descripción de los ejes")),
+                                             p(div(strong("Eje Y: "), em("Tasa de positividad de pruebas moleculares (promedio de 7 días)."), style = "color:blue")),
+                                             p(div(strong("Eje X: "), em("Días."), style = "color:blue"), "El primer día de la serie corresponde al 13/03/2020, fecha en la cual se reportó
+                                          el primer caso confirmado por COVID-19 en la región.")
+                                    ),
+                                    tabPanel("Resumen", "El semáforo COVID-19 de casos muestra
+                               el nivel de riesgo respecto al número total de contagiados
+                               por COVID-19.", br(), br(),
+                                             p("El color", strong("rojo"), "representa un nivel elevado de riesgo, en esta zona
+las precauciones aumentan. En esta zona el nivel y velocidad de contagio es mucho más elevada.
+Se recomienda salir de casa solo en casos excepcionales y tomando muy en cuenta las medidas
+de seguridad sanitaria. ", br(), br(),
+                                               "El color", strong("amarillo"), "representa un nivel de riesgo moderado. Aunque el riesgo aún se mantiene, se pueden realizar más
+actividades, siempre tomando en consideración las medidas de seguridad sanitaria.", br(), br(),
+                                               "El color", strong("verde"), "representa que el nivel de riesgo no es tan elevado respecto a los
+otros colores. En todo momento se deberían tomar en cuenta las medidas de seguridad sanitaria. 
+"))
+                                    )),
+                         tabPanel("Defunciones",
+                                  tabsetPanel(
+                                    tabPanel("Gráfico", dygraphOutput("dygraph_prov_new_deaths"),
+                                             h4(strong("Descripción de los ejes")),
+                                             p(div(strong("Eje Y: "), em("Tasa de positividad de pruebas moleculares (promedio de 7 días)."), style = "color:blue")),
+                                             p(div(strong("Eje X: "), em("Días."), style = "color:blue"), "El primer día de la serie corresponde al 13/03/2020, fecha en la cual se reportó
+                                          el primer caso confirmado por COVID-19 en la región.")
+                                    ),
+                                    tabPanel("Resumen", "El semáforo COVID-19 de casos muestra
+                               el nivel de riesgo respecto al número total de contagiados
+                               por COVID-19.", br(), br(),
+                                             p("El color", strong("rojo"), "representa un nivel elevado de riesgo, en esta zona
+las precauciones aumentan. En esta zona el nivel y velocidad de contagio es mucho más elevada.
+Se recomienda salir de casa solo en casos excepcionales y tomando muy en cuenta las medidas
+de seguridad sanitaria. ", br(), br(),
+                                               "El color", strong("amarillo"), "representa un nivel de riesgo moderado. Aunque el riesgo aún se mantiene, se pueden realizar más
+actividades, siempre tomando en consideración las medidas de seguridad sanitaria.", br(), br(),
+                                               "El color", strong("verde"), "representa que el nivel de riesgo no es tan elevado respecto a los
+otros colores. En todo momento se deberían tomar en cuenta las medidas de seguridad sanitaria. 
+"))
+                                  )))
+                         # tabPanel("Comparativo Casos",dygraphOutput("dygraph_prov_comparativo_casos")),
+                         # tabPanel("Comparativo Defunciones",dygraphOutput("dygraph_prov_comparativo_defunciones")),
+                         # tabPanel("Comparativo Positividad Molecular",dygraphOutput("dygraph_prov_comparativo_posimolecular")))
                      ),
                      
                      fluidRow(
-                       box(title = "Casos acumulados de Covid-19 (I)", dygraphOutput("plot3_prov"), textOutput("legend_plot3_prov")),
-                       box(title = "Casos acumulados de Covid-19 (II)", dygraphOutput("plot4_prov"), textOutput("legend_plot4_prov"))
+                       box(title = "Casos acumulados de Covid-19", dygraphOutput("plot3_prov"), textOutput("legend_plot3_prov")))
+                       # box(title = "Casos acumulados de Covid-19 (II)", dygraphOutput("plot4_prov"), textOutput("legend_plot4_prov"))
                        
-                     )
+
 )
